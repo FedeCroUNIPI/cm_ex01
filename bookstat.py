f27ceff045ec692ec40556492c0b51f786cfba82
Federico Crognale <f.crognale@studenti.unipi.it>
2022-09-24 20:57:30 +0200

    bookstat.py

---
+++ bookstat.py
@@ -0,0 +1,16 @@
+import argparse
+
+def process(file_path):
+    print(f'Opening input file {file_path}...')
+    with open(file_path, 'r') as input_file:
+        text = input_file.read()
+    print(text)
+    print('Done.')
+
+process('C:\Users\feder\Desktop\cm_ex01\oliver_twist.txt')
+
+#if __name__ == '__main__':
+ #   parser = argparse.ArgumentParser(description='Print some book statistics')
+  #  parser.add_argument('infile', type=str, help='path to the input file')
+   # args = parser.parse_args()
+    # process(args.infile)
