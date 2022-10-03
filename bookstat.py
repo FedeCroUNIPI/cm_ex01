6e0e7830e8b20d967570b037c705241a419b51f6
FedeCroUNIPI <f.crognale@studenti.unipi.it>
2022-10-03 12:07:05 +0200

    Initial import.

--- bookstat.py
+++ bookstat.py
@@ -1,16 +1,24 @@
 import argparse
+import string
+import time #elapsed time
+
+start=time.time() #start for elapsed time
+
+#input from command line
+if __name__ == '__main__':
+    parser=argparse.ArgumentParser(description="Print some book statistics")
+    parser.add_argument('input_file', type=str, help="path of input file") #help option
+    args=parser.parse_args()
 
 def process(file_path):
-    print(f'Opening input file {file_path}...')
-    with open(file_path, 'r') as input_file:
-        text = input_file.read()
+    """Open a file and read/print text inside."""
+    print(f'Opening input file: {file_path}')
+    with open(file_path,'r',encoding='utf-8') as book: #with: open and close file
+        text=book.read()
     print(text)
-    print('Done.')
 
-process('C:\Users\feder\Desktop\cm_ex01\oliver_twist.txt')
+#alphabet=dict(zip(string.ascii_lowercase, range(1,27)))
+process(args.input_file)
 
-#if __name__ == '__main__':
- #   parser = argparse.ArgumentParser(description='Print some book statistics')
-  #  parser.add_argument('infile', type=str, help='path to the input file')
-   # args = parser.parse_args()
-    # process(args.infile)
+end=time.time() #end for elapsed time
+print('Total elapsed time:', end-start)
