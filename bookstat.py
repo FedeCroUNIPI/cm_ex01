28c824b16ab8b36001022fcb27388a76b3cede34
FedeCroUNIPI <f.crognale@studenti.unipi.it>
2022-10-18 18:07:01 +0200

    Use a personal library, ask command line what to do with some if

--- bookstat.py
+++ bookstat.py
@@ -1,59 +1,59 @@
-#!/usr/bin/env python
-# Copyright (C) 2022 f.crognale@studenti.unipi.it
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+    Analyze a book
 
-"""1st assignment for the CMEPDA course, 2022/23.
+    For the moment there is not the option about skip preamble
 """
 
+#Import packages
 import argparse
-import string
+from argparse import RawTextHelpFormatter #for formatting help
 import time #elapsed time
 import matplotlib.pyplot as plt
 import numpy as np
 
-def process(file_path):
-    """Open a file and count letters"""
-    print(f'Opening input file: {file_path}')
-    #***It must skip preamble, .... : overstimated numbers of letters***
-    with open(file_path,'r',encoding='utf-8') as book: #with: open and close file
-        text=book.read().lower() #lower: uppercase to lowercase letters
-        #To not differentiate uppercase/lowercase and count twice
-        alphabet=dict.fromkeys(string.ascii_lowercase,0)
-        for i in string.ascii_lowercase:
-            counter=text.count(i)
-            alphabet[i]=counter
-        return alphabet
-
-def make_histo(alphabet_freq):
-    """Create a histogram"""
-    #Total number of letters
-    n_tot=0
-    for i in alphabet_freq:
-        n_tot+=alphabet_freq[i]
-    #Normalization
-    #for i in alphabet_freq:
-        #alphabet_freq[i]=alphabet_freq[i]/n_tot
-    plt.bar(alphabet_freq.keys(),alphabet_freq.values()) #x-axis, y-axis values
-    plt.show()
+import my_functions as my #my personal functions
 
 if __name__ == '__main__':
-    parser=argparse.ArgumentParser(description="Print some book statistics")
-    parser.add_argument('input_file', type=str, help="path of input file") #help option
+    parser=argparse.ArgumentParser(description="Print some book statistics", formatter_class=RawTextHelpFormatter)
+    parser.add_argument('input_file', metavar='File', type=str, help="path of input file") #help option
+    parser.add_argument('choice_stat', metavar='Choice', type=str, help='type of statistics\n'
+    '0 - all letters\n'
+    '0_histo - all letters with histogram\n'
+    'a,b,c,... - choose from alphabet\n'
+    'word - number of words\n'
+    'space - number of spaces')
     args=parser.parse_args()
-    start=time.time() #start for elapsed time
-    frequency=process(args.input_file)
-    end=time.time() #end for elapsed time
-    make_histo(frequency)
-    print('Totale elapsed time:',end-start)
+
+    #Rename for semplicity
+    choice=args.choice_stat
+    infile=args.input_file
+
+    t_start=time.time() #start for elapsed time
+    
+    #What to do
+    if choice == '0':
+        frequency=my.counter_letters(infile)
+        for i in frequency:
+            print(f'{i}\t{frequency[i]}')
+    elif choice == '0_histo':
+        frequency=my.counter_letters(infile)
+        for i in frequency:
+            print(f'{i}\t{frequency[i]}')
+        t_end=time.time() #end for elapsed time
+        #Here time elapsed time because histo does not stop running time
+        print(f'Total elapsed time: {t_end-t_start}')
+        my.make_histo(frequency)
+    elif choice == 'word':
+        word_result=my.counter_words(infile)
+        print(f'Total number of words: {len(word_result)}')
+    elif choice == 'space':
+        space_result=my.counter_spaces(infile)
+        print(f'Number of spaces: {space_result}')
+    else:
+        frequency=my.counter_letters(infile)
+        print(f'{choice}\t{frequency[choice]}')
+    
+    #Compute the elapsed time
+    if choice != '0_histo':
+        t_end=time.time() #end for elapsed time
+        print(f'Totale elapsed time: {t_end-t_start}')
